966cd7ab94d0098b2da8c8bb7cd6ccf56c033a7f
Hudson Stohl <hrstohl02@gmail.com>
2025-02-06 13:02:48 -0700

    page placeholders and routing working

--- src/app.jsx
+++ src/app.jsx
@@ -2,42 +2,60 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Login } from "./login/login";
+import { Choose } from "./choose/choose";
+import { Joined } from "./joined/joined";
+
 export default function App() {
   return (
-    <div className="bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">
-            Eventure
-          </a>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="choose.html">
-                Join
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="joined.html">
-                Group
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div className="app bg-dark text-light">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark bg-dark">
+            <div className="navbar-brand">Eventure</div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="choose">
+                  Join
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="joined">
+                  Group
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-      <main>Main stuff here!</main>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="/choose" element={<Choose />} />
+          <Route path="/joined" element={<Joined />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      <footer className="bg-dark text-white-50">
-        <hr />
-        <span className="text-reset">Hudson Stohl</span>
-        <br />
-        <a href="https://github.com/hstohl/startup">GitHub</a>
-      </footer>
-    </div>
+        <footer className="bg-dark text-white-50">
+          <hr />
+          <span className="text-reset">Hudson Stohl</span>
+          <br />
+          <a href="https://github.com/hstohl/startup">GitHub</a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
   );
 }
